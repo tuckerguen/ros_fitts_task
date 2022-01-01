38416f64707708bbade11155480cd52617792180
Tucker Guen <tkg11@case.edu>
2022-01-01 15:40:02 -0500

    add rest of 0.01 delay trials. Fix Traj saving 0.01 (home size) as target size for all trajectories. TODO: Edit trial trajs with correct target sizs

--- notebooks/debug.py
+++ notebooks/debug.py
@@ -3,13 +3,14 @@ import sys
 import warnings
 import os
 sys.path.append("../src")
-base = "C:/School/thesis/ros_fitts_task/trials"
-ts = "2021-12-29_16-49-08-519180"
+base = "/home/tucker/thesis/ros_workspace/src/fitts_task/trials"
+ts = "2021-12-30_14-30-18-403086"
 traj_path = [os.path.join(base, f, f2) for f in os.listdir(base) for f2 in os.listdir(os.path.join(base, f)) if f2 == ts]
 if traj_path:
     with open(os.path.join(traj_path[0], "trial.pkl"), 'rb') as f:
         trial = dill.load(f)
-        trial.replay(speed=3)
+        # trial.replay(speed=3)
+        trial.plot()
 else:
     warnings.warn(f"No trial for {ts}")
 
---
+++ trials/0.00/tucker_0.00_0.050/2021-12-29_15-15-32-143518/pyfiles/mocap.py
@@ -0,0 +1,122 @@
+#!/home/tucker/anaconda3/bin/python3
+import sys
+
+import dill
+import numpy as np
+import pygame
+import quaternion
+import rospy
+import rosbag
+
+sys.path.append('/scripts')
+
+from geometry_msgs.msg import PoseArray
+from util import is_pygame_running, render_lines_of_text
+
+
+class MocapTracker:
+    def __init__(self, bag_path):
+        # Set up ROS
+        rospy.init_node('read_polaris', anonymous=True)
+        rospy.Subscriber("polaris_sensor/targets", PoseArray, self.polaris_callback, queue_size=1)
+
+        self.bag = rosbag.Bag(bag_path, "w")
+
+        # Data tracking and control flow
+        self.collect_data = False
+        self.marker_visible = False
+
+        # Calibration
+        self.is_calibrate = True
+        self.cb_pt = np.nan  # Calibration point
+        self.cb_q = np.nan  # Calibration quaternion
+        self.T = None  # Computed transformation matrix from calibration
+
+        # Default framerate, doesn't matter during calibration
+        self.framerate = 60
+
+    def get_mocap_pt(self):
+        if not np.any(np.isnan(self.cb_pt)):
+            pt = np.matmul(self.T, self.cb_pt)[1:3]
+            pt = pt[::-1]
+            return pt
+        else:
+            return None
+
+    def calibrate(self, time):
+        """
+        With the marker placed at the origin of the workspace (world) coordinate frame, we collect
+        some data to create the camera to world matrix. We invert that and apply it to 3D points
+        collected from the camera in order to map those points to xy plane in camera space. Then, given
+        the known size of the workspace and the known size of the screen, we directly map those points
+        in the xy plane to the screen's pixel coordinates
+        """
+        pygame.init()
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        pygame.display.set_caption('Fitts task')
+        pygame.font.init()
+        render_lines_of_text(screen, ["Running calibration"])
+
+        pg_clock = pygame.time.Clock()
+
+        calib_pts = []
+        calib_qs = []
+
+        dt = 1 / self.framerate
+        wait_steps = self.framerate * time
+        i = 0
+
+        while i < wait_steps:
+            screen.fill((0, 0, 0))
+            if np.any(np.isnan(self.cb_pt)) or np.any(np.isnan(self.cb_q)):
+                render_lines_of_text(screen, ["Cannot see marker"])
+            else:
+                # print(i*dt, self.cb_q, self.cb_pt)
+                render_lines_of_text(screen, [f"Calibrating for 5s. t={i * dt:.3f}"])
+                calib_pts.append(self.cb_pt)
+                calib_qs.append(self.cb_q)
+                i += 1
+
+            pygame.display.flip()
+            pygame.display.update()
+            pg_clock.tick(self.framerate)
+
+            if not is_pygame_running():
+                return False
+
+        pygame.quit()
+        self._make_transformation_matrix(calib_pts, calib_qs)
+
+        return True
+
+    def _make_transformation_matrix(self, calib_pts, calib_qs):
+        # Construct the transformation matrix
+        ws_origin = np.mean(np.array(calib_pts), axis=0)
+        ws_q = np.quaternion(np.mean(np.array(calib_qs)))
+
+        # print(ws_origin, ws_q)
+        # Transformation matrix from camera frame workspace frame
+        rot_mat = quaternion.as_rotation_matrix(ws_q.inverse())
+
+        # Create the full transformation matrix
+        T = np.zeros((4, 4))
+        T[:3, :3] = rot_mat
+        T[:3, 3] = np.matmul(rot_mat, -ws_origin[:3])
+        T[3, 3] = 1
+        self.T = T
+
+    def polaris_callback(self, pose_array):
+        self.bag.write("polaris_sensor/targets", pose_array)
+
+        pos = pose_array.poses[0].position
+        position = np.array([pos.x, pos.y, pos.z, 1])
+        q = pose_array.poses[0].orientation
+        orientation = np.quaternion(q.w, q.x, q.y, q.z)
+        self.cb_pt = position
+        self.cb_q = orientation
+
+    def save(self, fn):
+        with open(fn, "wb") as f:
+            dill.dump(self, f)
+
+
